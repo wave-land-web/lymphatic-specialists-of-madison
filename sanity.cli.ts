8496d96ef16200b02ddf4ee956ea7468da43c373
Josh Nussbaum <64871335+joshnussbaum89@users.noreply.github.com>
2025-07-03 14:28:49 -0500

    feat(app): deploy studio, size imported image

---
+++ sanity.cli.ts
@@ -0,0 +1,8 @@
+import { defineCliConfig } from 'sanity/cli'
+export default defineCliConfig({
+  api: {
+    projectId: 'hr4xqyhv',
+    dataset: 'production',
+  },
+  studioHost: 'madison-lymphatics-and-manual-therapy/admin',
+})
--- sanity.config.ts
+++ sanity.config.ts
@@ -4,8 +4,8 @@ import { schema } from './src/sanity/schemaTypes'
 import { structure } from './src/sanity/structure'
 
 export default defineConfig({
-  projectId: import.meta.env.PUBLIC_SANITY_PROJECT_ID,
-  dataset: import.meta.env.PUBLIC_SANITY_DATASET,
+  projectId: 'hr4xqyhv',
+  dataset: 'production',
   plugins: [structureTool({ structure })],
   schema,
   name: 'madison-lymphatics-and-manual-therapy',
